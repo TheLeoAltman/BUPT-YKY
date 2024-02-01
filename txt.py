b0818b315aee4a2cc1145359601051680053c4aa
TheLeoAltman <156597243+TheLeoAltman@users.noreply.github.com>
2024-02-01 18:03:14 +0800

    Delete txt.py

--- txt.py
+++
@@ -1,7 +0,0 @@
-import shutil
-
-class txt:
-    def __init__(self, path, filename):
-        out_path = 'trans/' + filename
-        in_path = path + '/' + filename
-        shutil.copyfile(in_path, out_path)
